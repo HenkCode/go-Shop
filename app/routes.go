5b605462ffeabc2778cb69faa0891e3b0ac5d808
henk <barbarnorem@gmail.com>
2024-10-26 21:52:59 +0700

    menambah configurasi DB and GORM

--- app/routes.go
+++ app/routes.go
@@ -1,7 +1,12 @@
 package app
 
-import "github.com/HenkCode/go-Shop/app/handlers"
+import (
+	"github.com/HenkCode/go-Shop/app/handlers"
+	"github.com/gofiber/fiber/v2"
+)
 
 func (server *Server) initializeRoutes() {
+	server.Router = fiber.New()
+	
 	server.Router.Get("/", handlers.Home)
 }
--- app/server.go
+++ app/server.go
@@ -3,33 +3,93 @@ package app
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+type AppConfig struct {
+	Name string
+	Env  string
+	Port string
+}
+
+type DBConfig struct {
+	Host     string
+	User     string
+	Password string
+	Name     string
+	Port     string
+}
+
 type Server struct {
-	DB 	   *gorm.DB
+	DB     *gorm.DB
 	Router *fiber.App
 }
 
-func (server *Server) Initialize() {
-	fmt.Println("Server Go-Shop berjalan...")
+func (server *Server) Initialize(appConfig AppConfig, dbConfig DBConfig) {
+	fmt.Println("Server " + appConfig.Name + " Berjalan...")
 
-	server.Router = fiber.New()
+	server.initializeDB(dbConfig)
 	server.initializeRoutes()
 }
+
+
 func (server *Server) Run(address string) {
 	fmt.Printf("Berjalan di Port: %s", address)
-
+	
 	log.Fatal(server.Router.Listen(address))
+}
+
+func (server *Server) initializeDB(dbConfig DBConfig) {
+	var err error
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Name)
+	fmt.Println("DSN:", dsn)
+	server.DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed connecting to the database server.")
+	}
+	
+	for _, model := range RegisterModels() {
+		err = server.DB.Debug().AutoMigrate(model.Model)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	fmt.Println("Database migrated successfully.")
+}
+
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
 }
 
 func Run() {
 	var server = Server{}
+	var appConfig = AppConfig{}
+	var dbConfig = DBConfig{}
 
-	server.Initialize()
-	server.Run(":8080")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error on Loading .env file.")
+	}
 
+	appConfig.Name = getEnv("APP_NAME", "go-Shop")
+	appConfig.Env  = getEnv("APP_ENV", "dev")
+	appConfig.Port = getEnv("APP_PORT", "9000")
 
+	dbConfig.Host 	  = getEnv("DB_HOST", "localhost")
+	dbConfig.Name     = getEnv("DB_NAME", "go-shop")
+	dbConfig.User     = getEnv("DB_USER", "root")
+	dbConfig.Password = getEnv("DB_PASSWORD", "")
+	dbConfig.Port     = getEnv("DB_PORT", "5432")
+
+	server.Initialize(appConfig, dbConfig)
+	server.Run(":" + appConfig.Port)
 }
